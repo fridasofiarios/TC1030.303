2ea755754b85acafafdf8f47003b71e1ee45b9a2
fridasofiarios <a01746617@itesm.mx>
2021-05-20 11:37:29 -0500

    s

---
+++ 1-Herencia/1-simple/tempCodeRunnerFile.d
@@ -0,0 +1,1 @@
+tempCodeRunnerFile.o: tempCodeRunnerFile.cpp
---
+++ 1-Herencia/2-Multiple/multiple.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+    public:
+    void display()
+    {
+        cout << "Display de clase A" << endl;
+    }
+};
+class B
+{
+    public:
+    void display()
+    {
+        cout << "Display de clase B" << endl;
+    }
+};
+class C : public A , public B
+{
+    
+};
+
+int main()
+{
+    C obj;
+
+    obj.A::display();
+    obj.B::display();
+
+    return 0;
+}
